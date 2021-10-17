d54bc2828f17e3dcaa135cb181d294a20dc9b07a
Storiwr <storiwr@users.noreply.github.com>
2021-10-17 15:27:07 -0400

    added environment object and publisher

--- iDine/ItemDetail.swift
+++ iDine/ItemDetail.swift
@@ -8,11 +8,15 @@
 import SwiftUI
 
 struct ItemDetail: View {
+    @EnvironmentObject var order: Order
     let item: MenuItem
+    
     var body: some View {
         VStack {
             ZStack(alignment: .bottomTrailing) {
                 Image(item.mainImage)
+                    .resizable()
+                    .scaledToFit()
                 Text("Photo: \(item.photoCredit)")
                     .padding(4)
                     .background(Color.black)
@@ -22,6 +26,12 @@ struct ItemDetail: View {
             }
             Text(item.description)
                 .padding()
+            
+            Button("Order This") {
+                order.add(item: item)
+            }
+            .font(.headline)
+            
             Spacer()
         }
         .navigationTitle(item.name)
@@ -33,6 +43,7 @@ struct ItemDetail_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView {
         ItemDetail(item: MenuItem.example)
+                .environmentObject(Order())
         }
     }
 }
