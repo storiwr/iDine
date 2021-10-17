1998ceb562c6e916e9fcd33d02a4667f6cced836
Storiwr <storiwr@users.noreply.github.com>
2021-10-17 14:23:00 -0400

    updated with pretty food type badges

--- iDine/ItemRow.swift
+++ iDine/ItemRow.swift
@@ -8,9 +8,34 @@
 import SwiftUI
 
 struct ItemRow: View {
+    let colors: [String: Color] = ["D": .purple, "G": .black, "N": .red,"S": .blue, "V": .green]
+    
     let item: MenuItem
     var body: some View {
-        Text(item.name)
+        HStack {
+            Image(item.thumbnailImage)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.gray, lineWidth: 2))
+            
+            VStack(alignment: .leading) {
+                Text(item.name)
+                    .font(.headline)
+                Text("$\(item.price)")
+            }
+            Spacer()
+            
+            ForEach(item.restrictions, id:\.self) {restriction in
+                Text(restriction)
+                    .font(.caption)
+                    .fontWeight(.black)
+                    .padding(5)
+                    .background(colors[restriction, default: .black])
+                    .clipShape(Circle())
+                    .foregroundColor(.white)
+            }
+           
+        }
+        
     }
 }
 
